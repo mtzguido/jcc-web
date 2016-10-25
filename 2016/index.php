8842e81c593db967d820edc228b89982c048b803
Guido Martínez <mtzguido@gmail.com>
2016-10-25 10:56:37 -0300

    no depender de google para jquery

--- 2016/index.php
+++ 2016/index.php
@@ -70,7 +70,7 @@
 <link rel="shortcut icon" href="favicon.ico">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script>
+<script src="jquery.min.js" type="text/javascript"></script>
 <!-- lighbox -->
 <script src="lightbox.js" type="text/javascript"></script>
 <script type="text/javascript">
